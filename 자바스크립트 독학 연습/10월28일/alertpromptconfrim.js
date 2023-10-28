b7db44515df243be5a6dd075fb697a35458ed54b
CroppedEyelid <lee940706@gmail.com>
2023-10-28 21:45:11 +0900

    js공부.10월28일

---
+++ 자바스크립트 독학 연습/10월28일/alertpromptconfrim.js
@@ -0,0 +1,28 @@
+// const name = prompt("이름을 입력하세요.");
+// alert("환영합니다," + name + "님");
+
+// let num = 10;
+// num += 5;
+
+// console.log(num);
+
+// let number = 10;
+// let result = ++number;
+
+// console.log(result);
+
+// console.log(10 > 5);
+// console.log(10 == 5);
+// console.log(10 != 5);
+
+const age = 19;
+
+if (age > 19) {
+  console.log(`환영 합니다`);
+} else if (age === 19) {
+  console.log("수능 잘 치세요");
+} else {
+  console.log(`안녕히가세요.`);
+}
+
+console.log("00------------------00");
---
+++ 자바스크립트 독학 연습/10월28일/자료형.js
@@ -0,0 +1,30 @@
+// const name = "Mike"; //문자형 String
+// const age = 30;
+
+// const name1 = "Mike";
+// const name2 = "mike";
+// const name3 = `Mike`;
+
+// const message = "I'm a boy.";
+// const message2 = "I'm a boy.";
+
+// const message3 = `My name is ${name}`;
+// const message4 = `나는 ${age + 1}살 입니다.`;
+
+// console.log(message4);
+
+// const age = 30; // 숫자형 Number
+// const PI = 3.14;
+
+// console.log(1 + 2); //더하기
+// console.log(10 - 3); //빼기
+// console.log(3 * 2); //곱하기
+// console.log(6 / 3); //나누기
+// console.log(6 % 4); //% 나머지
+
+// const x = 1 / 0;
+// console.log(x);
+
+// const name = "Mike";
+// const y = name / 2;
+// console.log(y);
